7e8b78c09d38080b24558361a9cf776ee3c9f5ec
duddaarants <meduardaarantes66@gmail.com>
2021-10-27 19:25:10 -0300

    modificações

--- app/src/main/java/com/example/novo/MainActivity.java
+++ app/src/main/java/com/example/novo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.novo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +30,12 @@ btnEntrar=findViewById(R.id.btnEntrar);
 btnEntrar.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
+        startActivity(new Intent(getApplicationContext(),MainActivity2.class));
+        finish();
+
+        Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+
+        startActivity(intent);
 
         String usuario, senha;
 
@@ -36,12 +43,10 @@ btnEntrar.setOnClickListener(new View.OnClickListener() {
         senha = edtSenha.getText().toString();
 
         if (edtUsuario.equals("maria") && edtSenha.equals("etecia")){
-
             Toast.makeText(getApplicationContext(),
-
                     "Bem vindo!",
-
                     Toast.LENGTH_SHORT).show();
+
         }else{
             Toast.makeText(getApplicationContext(),
                     "Usuário ou senha inválidos!",
